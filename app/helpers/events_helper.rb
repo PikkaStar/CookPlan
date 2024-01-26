1ab9348e768eaf06f281cf48ff6910d2af65a9cd
PikkaStar <yokotaka1226@gmail.com>
2024-01-26 05:45:55 +0000

    enum

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,4 +1,5 @@
 module EventsHelper
+  # 送られてきたデータを日本語で表示するためにja.ymlファイルで参照する
   def display_cooking_number(event)
     I18n.t("activerecord.attributes.event.cooking_numbers.#{event.cooking_number}")
   end
